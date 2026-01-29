958fb33188fb1b19fc577bae7b8dbee0cadeec12
Logan <lbransco@scholar.yc.edu>
2026-01-29 11:16:09 -0700

    Create handle_form.php

---
+++ handle_form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Results</title>
+</head>
+<body>
+
+<h2>Form Results</h2>
+
+<?php
+
+    echo "First Name: " . $_POST['first_name'] . "<br>";
+    echo "Last Name: " . $_POST['last_name'];
+
+
+?>
+
+</body>
+</html>
