2cb73136e359eda9ba71c835744ae2159fb25874
Logan <lbransco@scholar.yc.edu>
2026-03-08 15:08:36 -0700

    Create main_index.php

---
+++ main_index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contact Form</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h2>Contact Information Form</h2>
+
+<form action="process.php" method="post">
+
+First Name:<br>
+<input type="text" name="first_name"><br><br>
+
+Last Name:<br>
+<input type="text" name="last_name"><br><br>
+
+Address:<br>
+<input type="text" name="address"><br><br>
+
+City:<br>
+<input type="text" name="city"><br><br>
+
+State:<br>
+<input type="text" name="state"><br><br>
+
+Phone Number:<br>
+<input type="text" name="phone"><br><br>
+
+Email Address:<br>
+<input type="email" name="email"><br><br>
+
+<input type="submit" value="Submit">
+
+</form>
+
+</body>
+</html>
